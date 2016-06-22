102ea33dc07006c8ff55d6fdb9962e75b0b539ef
Shubheksha Jalan <t-shjala@microsoft.com>
2016-06-22 13:01:12 +0530

    Added ProductListComponent & displayed table

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,8 +1,15 @@
 import {Component} from 'angular2/core'
-
+import {ProductListComponent} from './products/product-list.component'
 @Component({
     selector: 'pm-app',
-    template: '<h1>{{pageTitle}}<h1>'
+    template: `
+    <div>
+    <h1>{{pageTitle}}</h1>
+    <pm-products></pm-products>
+    </div>
+    `,
+    directives: [ProductListComponent]
+
 })
 
 export class AppComponent {
